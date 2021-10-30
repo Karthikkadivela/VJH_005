912547b97877bb6d1e26e56f40a946f5691adcaa
KarthikKN007 <karthikkadivela@gmail.com>
2021-10-30 11:46:08 +0530

    final submit

--- add_schemes.php
+++ add_schemes.php
@@ -98,7 +98,7 @@
         </div>
         <div class="col-4"></div>
     </div>
-    <footer class="bg-dark text-center text-white">
+    <footer class="bg-dark text-center text-white fixed-bottom">
             <!-- Grid container -->
             <div class="container p-2 pb-0">
               <!-- Section: Social media -->
